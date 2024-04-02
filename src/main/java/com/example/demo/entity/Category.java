3c7558d20980f2799783c4cd23f9858a3af5e659
sattorovasror967@gmail.com <asrorgithubtest@gmail.com>
2024-04-02 16:41:39 +0500

    created classes

---
+++ src/main/java/com/example/demo/entity/Category.java
@@ -0,0 +1,24 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String name;
+
+    @OneToMany
+    @JoinColumn(name = "category_id")
+    private List<Product> products;
+}
